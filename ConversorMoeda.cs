30d8e1ec193b8bb5ecad4f4bdcf342a7723d1bb2
Josué <gutillaj@gmail.com>
2023-01-21 10:21:05 -0400

    convertor de dolar usando classe static

---
+++ ConversorMoeda.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex_dolar
+{
+    public class ConversorMoeda
+    {
+        public static int Iof = 6;
+
+        public static double Dolar;
+
+        public static double ConersorDolarReal(double dolar, double quantidade)
+        {
+            double conversor = dolar * quantidade;
+
+            return conversor + conversor * Iof / 100;
+
+            
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Globalization;
+
+namespace ex_dolar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Qual é a cotação do dólar? ");
+            double dolar = double.Parse(Console.ReadLine());
+            Console.Write("Quantos dólares você vai comprar? ");
+            double quant = double.Parse(Console.ReadLine());
+
+            double conversor = ConversorMoeda.ConersorDolarReal(dolar, quant);
+
+            Console.WriteLine();
+            Console.WriteLine("Valor a ser pago em reais = " + conversor.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
